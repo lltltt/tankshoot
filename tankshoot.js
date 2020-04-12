ebc50e4bdb33e5175793e7cd668f7ca444d1f112
simcap <simcap@fastmail.com>
2020-04-12 13:54:37 +0200

    moving tank

--- tankshoot.js
+++ tankshoot.js
@@ -1,32 +1,92 @@
-window.addEventListener('keydown', this.move,false);
-
-var tank = new Image();
+var game = new Game();
 
 function init() {
-    tank.src = 'https://i.imgur.com/N2Ck06y.png';
-    window.requestAnimationFrame(draw);
+	if(game.init())
+		game.start();
 }
 
-function draw() {
-    var ctx = document.getElementById('canvas').getContext('2d');
+function Drawable() {
+	this.init = function(x, y) {
+		this.x = x;
+		this.y = y;
+	}
+	this.speed = 0;
+	this.canvasWidth = 0;
+	this.canvasHeight = 0;
 
-
-    var time = new Date();
-    ctx.drawImage(tank, 0, 400);
-
-    window.requestAnimationFrame(draw);
+	this.draw = function() {};
 }
 
-function move(event) {
-    if (event.keyCode == 39) {
-        var ctx = document.getElementById('canvas').getContext('2d');
-        ctx.translate(10, 0)    
-    } 
+function Background() {
+    this.background = new Image();
+    this.background.src = "https://storage.needpix.com/rsynced_images/gray-rocky-background.jpg";
 
-    if (event.keyCode== 37) {
-        var ctx = document.getElementById('canvas').getContext('2d');
-        ctx.translate(-10, 0)            
-    }
+	this.speed = 3;
+	this.draw = function() {
+		this.x = this.x - this.speed;
+		this.context.drawImage(this.background, this.x, this.y);
+		this.context.drawImage(this.background, this.x + this.background.width, this.y);
+
+		if (this.x <= -this.canvasWidth)
+			this.x = 0;
+	};
+}
+Background.prototype = new Drawable();
+
+function Tank() {
+    this.tank = new Image();
+    this.tank.src = "https://i.imgur.com/N2Ck06y.png";
+
+    this.draw = function() {
+        this.context.drawImage(this.tank, this.x, this.y);
+    };
+}
+Tank.prototype = new Drawable();
+
+function Game() {
+	this.init = function() {
+		this.bgCanvas = document.getElementById('canvas');
+		if (this.bgCanvas.getContext) {
+			this.bgContext = this.bgCanvas.getContext('2d');
+
+            Background.prototype.context = this.bgContext;
+			Background.prototype.canvasWidth = this.bgCanvas.width;
+			Background.prototype.canvasHeight = this.bgCanvas.height;
+
+            Tank.prototype.context = this.bgContext;
+			Tank.prototype.canvasWidth = this.bgCanvas.width;
+			Tank.prototype.canvasHeight = this.bgCanvas.height;
+
+            this.background = new Background();
+            this.background.init(0,0);
+            
+            this.tank = new Tank();
+            this.tank.init(0, 300);
+
+			return true;
+		} else {
+			return false;
+		}
+	};
+
+	this.start = function() {
+		animate();
+	};
 }
 
-init();
+function animate() {
+	requestAnimFrame( animate );
+    game.background.draw();
+    game.tank.draw();
+}
+
+window.requestAnimFrame = (function(){
+	return  window.requestAnimationFrame   ||
+			window.webkitRequestAnimationFrame ||
+			window.mozRequestAnimationFrame    ||
+			window.oRequestAnimationFrame      ||
+			window.msRequestAnimationFrame     ||
+			function(callback, element){
+				window.setTimeout(callback, 1000 / 60);
+			};
+})();
